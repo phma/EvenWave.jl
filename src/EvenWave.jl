46a8307b03b3de097c72341ef802c186fe312c95
Pierre Abbat <phma@bezitopo.org>
2024-01-01 20:42:57 +0000

    Define rotx

--- src/EvenWave.jl
+++ src/EvenWave.jl
@@ -1,10 +1,14 @@
 module EvenWave
-export φz
+export φz,rotx
 
 using Quaternions
 
 φturn=(√5-1)*π/2
 
 φz=quat(cos(φturn/2),0,0,sin(φturn/2))
+
+function rotx(θ::AbstractFloat)
+  quat(cos(θ),sin(θ),0,0)
+end
 
 end # module EvenWave
